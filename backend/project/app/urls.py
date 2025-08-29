58b5f93b0fd786b1177ebbd53f7dd1251cf1ff9e
Shikazu777 <dhanrax@gmail.com>
2025-08-29 19:59:35 +0530

    code updated

--- backend/project/app/urls.py
+++ backend/project/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from app import views
 
 urlpatterns = [
-    path("",views.indes,name='index'),
-    path("/login",views.login,name="login")
+    path("",views.index,name='index'),
+    path("login",views.login,name="login")
 ]
 
 # ""=="/"=="localhost"==http://127.0.0.0:8000/
