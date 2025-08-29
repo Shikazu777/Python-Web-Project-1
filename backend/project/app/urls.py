a309af78f27a600dc0b33447e8bd3904f99646a3
Shikazu777 <dhanrax@gmail.com>
2025-08-29 18:29:52 +0530

    updated

--- backend/project/app/urls.py
+++ backend/project/app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 urlpatterns = [
-    path("",views.indes,name='index')
+    path("",views.indes,name='index'),
+    path("/login",views.login,name="login")
 ]
 
 # ""=="/"=="localhost"==http://127.0.0.0:8000/
--- backend/project/app/views.py
+++ backend/project/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request)
+def index(request):
     return render(request,"index.html")
+
+def login(request):
+    return render(request,"login.html")
