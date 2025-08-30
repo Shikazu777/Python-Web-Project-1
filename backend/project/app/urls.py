ea5fdc44685f8570875c5dee39f210999ef6b1c7
Shikazu777 <dhanrax@gmail.com>
2025-08-30 13:58:33 +0530

    updated

--- backend/project/app/urls.py
+++ backend/project/app/urls.py
@@ -3,7 +3,7 @@ from app import views
 
 urlpatterns = [
     path("",views.index,name='index'),
-    path("login",views.login,name="login")
+    path("login",views.login,name='login')
 ]
 
 # ""=="/"=="localhost"==http://127.0.0.0:8000/
