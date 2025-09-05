fae486df094aa741e53a2dbb9d968eced64af322
Shikazu777 <dhanrax@gmail.com>
2025-09-05 16:29:28 +0530

    updated

--- frontend/app/src/components/Screens/Home.jsx
+++ frontend/app/src/components/Screens/Home.jsx
@@ -26,7 +26,72 @@ function Home() {
   }
 
   return(
-    <div>Home</div>
+    <>
+    <div className="container">
+        <h1>Application Details</h1>
+        <hr />
+        <table className='table table-bordered'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Applicant Name</th>
+                    <th>Applicant Name</th>
+                    <th>Gender</th>
+                    <th>District</th>
+                    <th>State</th>
+                    <th>Pincode</th>
+                    <th>Ownership</th>
+                    <th>Govt ID Type</th>
+                    <th>ID Number</th>
+                    <th>Category</th>
+                    <th>Load Applied</th>
+                    <th>Date of Application</th>
+                    <th>Status</th>
+                    <th>Reviewer ID</th>
+                    <th>Reviewer Name</th>
+                    <th>Reviewer Comments</th>
+                    <th>Edit</th>
+ 
+                </tr>
+            </thead>
+
+            
+                <tbody>
+  {data?.map((connection) => (
+    <tr key={connection.id}>
+      <td>{connection.id}</td>
+      <td>{connection.Applicant.Applicant_Name}</td>
+      <td>{connection.Applicant.Gender}</td>
+      <td>{connection.Applicant.District}</td>
+      <td>{connection.Applicant.State}</td>
+      <td>{connection.Applicant.Pincode}</td>
+      <td>{connection.Applicant.Ownership}</td>
+      <td>{connection.Applicant.Govt_ID_Type}</td>
+      <td>{connection.Applicant.ID_Number}</td>
+      <td>{connection.Applicant.Category}</td>
+      <td>{connection.Applicant.Load_Applied}</td>
+      <td>{connection.Applicant.Date_of_Application}</td>
+      <td>{connection.Applicant.Status}</td>
+      <td>{connection.Applicant.Reviewer_ID}</td>
+      <td>{connection.Applicant.Reviewer_Name}</td>
+      <td>{connection.Applicant.Reviewer_Comments}</td>
+      <td>
+        <Link
+          type="button"
+          to={`/editApplicant/${connection.id}`}
+          className="btn btn-outline-success text-white"
+        >
+          <i className="fa-solid fa-pen-to-square"></i>
+        </Link>
+      </td>
+    </tr>
+  ))}
+</tbody>
+
+          
+        </table>
+    </div>
+    </>
   )
 }
 
